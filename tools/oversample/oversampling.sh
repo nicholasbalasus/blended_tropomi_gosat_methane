1b46126deef6b731a441ed7ff0d4a8d426740878
nicholasbalasus <nicholasbalasus@g.harvard.edu>
2023-04-04 09:49:33 -0400

    flexible file paths

--- tools/oversample/oversampling.sh
+++ tools/oversample/oversampling.sh
@@ -4,8 +4,8 @@
 #-----------------------------------------------------------------
 # Set path
 #-----------------------------------------------------------------
-set Input_Dir   = "/n/holylfs05/LABS/jacob_lab/nbalasus/blended_tropomi_gosat_methane/oversample/input/"
-set Output_Dir  = "/n/holylfs05/LABS/jacob_lab/nbalasus/blended_tropomi_gosat_methane/oversample/output/"
+set Input_Dir   = "${StorageDir}/oversample/input/"
+set Output_Dir  = "${StorageDir}/oversample/output/"
 
 #-----------------------------------------------------------------
 # Output resolution you want
